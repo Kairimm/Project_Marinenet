68ec28deda06df2ad63fa6e991026c5f39ea218b
Lotte De vries <ldevries38@st.noorderpoort.nl>
2022-11-25 11:02:21 +0100

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,30 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    colors : {
+      darkgreen : '#289500',
+      lightgrey : '#808080',
+      darkgrey : '#272727',
+      aqua : '#54B0C6',
+      red : '#E04343',
+      lightgreen : '#54C673',
+      orange : '#FA7706',
+      green : '#77FA06',
+      blue : '#45B7FF'
+    },
+    extend: {
+      screens : {
+        'xs' : '376px',
+        
+        'sm': '421px',
+  
+        'md': '821px',
+      },
+      fontFamily : {
+        helvatica  : ['Helvatica Neue', 'sans-serif']
+      }
+    },
+  },
+  plugins: [],
+}
